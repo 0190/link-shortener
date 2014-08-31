624f7e6f45ac8b546a9751a5910e7bffaff56384
Maria Klimkina <0910.nanami@gmail.com>
2014-08-31 21:40:18 +0300

    link shortener and redirect added

--- link_shortener.py
+++ link_shortener.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+url_list = ['http://ogoloda.li']
 
 @app.route('/')
 def template_renderer(name=None):
@@ -8,7 +9,12 @@ def template_renderer(name=None):
 
 @app.route('/urls', methods=['POST']) #fix this
 def store_urls():
-	url = request.form['url']
+	url_list.append(request.form['link'])
+	return 'OK!'
+
+@app.route('/l/<id>')
+def return_link(id=None):
+	return redirect(url_list[int(id)])
 
 if __name__ == '__main__':
     app.run()
