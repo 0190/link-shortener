33fda2ddfc689f5199fc1d6ee8f69a3aebf2dd50
Maria Klimkina <0910.nanami@gmail.com>
2014-09-01 15:20:06 +0300

    now returns fixed link

--- link_shortener.py
+++ link_shortener.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, Markup
 
 app = Flask(__name__)
 url_list = ['http://ogoloda.li']
+host_name = 'http://127.0.0.1:5000'
 
 @app.route('/')
 def template_renderer(name=None):
@@ -10,9 +11,9 @@ def template_renderer(name=None):
 @app.route('/urls', methods=['POST']) #fix this
 def store_urls():
 	url_list.append(request.form['link'])
-	url = url_for('return_link', id=str(len(url_list) - 1))
-	full_url = 'http://127.0.0.1:5000' + url
-	return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (full_url, full_url)
+	url = url_for('return_link', id=str(len(url_list) - 1), _external=True)
+	# full_url = host_name + url
+	return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (url, url)
 
 @app.route('/l/<id>')
 def return_link(id=None):
