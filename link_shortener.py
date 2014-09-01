a2d2d9c0c65adf63b73b0bafba09b5d534479109
Maria Klimkina <0910.nanami@gmail.com>
2014-09-01 15:21:02 +0300

    refactor

--- link_shortener.py
+++ link_shortener.py
@@ -12,7 +12,6 @@ def template_renderer(name=None):
 def store_urls():
 	url_list.append(request.form['link'])
 	url = url_for('return_link', id=str(len(url_list) - 1), _external=True)
-	# full_url = host_name + url
 	return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (url, url)
 
 @app.route('/l/<id>')
