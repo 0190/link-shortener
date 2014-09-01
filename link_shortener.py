57f667268e02d6f902c9f96b48b8ff5dba5a7434
Maria Klimkina <0910.nanami@gmail.com>
2014-09-01 15:12:48 +0300

    now returns the shortened link

--- link_shortener.py
+++ link_shortener.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, Markup
 
 app = Flask(__name__)
 url_list = ['http://ogoloda.li']
 
 @app.route('/')
 def template_renderer(name=None):
-    return render_template('link_shortener.html', name=name)
+	return render_template('link_shortener.html', name=name)
 
 @app.route('/urls', methods=['POST']) #fix this
 def store_urls():
 	url_list.append(request.form['link'])
-	return 'OK!'
+	url = url_for('return_link', id=str(len(url_list) - 1))
+	full_url = 'http://127.0.0.1:5000' + url
+	return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (url_list[-1], full_url)
 
 @app.route('/l/<id>')
 def return_link(id=None):
