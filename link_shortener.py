578fcc22eb42bfdf2860146ebb5e2d5227fb61be
Maria Klimkina <0910.nanami@gmail.com>
2014-09-02 15:40:07 +0300

    added error handling and selenium testing

--- link_shortener.py
+++ link_shortener.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, Markup
+from urlparse import urlparse
 
 app = Flask(__name__)
 url_list = ['http://ogoloda.li']
+valid_protocols = ['http', 'https']
 
 @app.route('/')
 def template_renderer(name=None):
@@ -9,13 +11,24 @@ def template_renderer(name=None):
 
 @app.route('/urls', methods=['POST']) #fix this
 def store_urls():
-	url_list.append(request.form['link'])
-	url = url_for('return_link', id=str(len(url_list) - 1), _external=True)
-	return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (url, url)
+	link_name = request.form['link']
+	parsed_link = urlparse(link_name)
+	if parsed_link.scheme in valid_protocols and parsed_link.netloc:
+		url_list.append(link_name)
+		url = url_for('return_link', id=str(len(url_list) - 1), _external=True)
+		return Markup('<h1>Here\'s your new link!</h1><a href=" %s " > %s </a>') % (url, url)
+	else:
+		return redirect('/error')
 
 @app.route('/l/<id>')
 def return_link(id=None):
 	return redirect(url_list[int(id)])
+
+@app.route('/error')
+def error():
+	return Markup('<h1>There seems to be a problem with your link</h1> \
+		<p>Note that we only work with http and https links.</p> \
+		<p>Please check the link format and <a href="/">try again</a>.</p>')
 
 if __name__ == '__main__':
     app.run()
---
+++ test/test.py
@@ -0,0 +1,35 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from urlparse import urlparse
+
+host = "http://127.0.0.1:5000/"
+valid_protocols = ['http', 'https']
+
+if len(sys.argv) > 1 and sys.argv[1] == ('--host' or '-h'):
+	host = sys.argv[2]
+
+links = {'twitter.com': 'Twitter', 'http://google.com': 'Google', \
+		 'http://docs.seleniumhq.org': 'Selenium - Web Browser Automation', \
+		 'www.meetup.com/Selenium-Israel/': 'Selenium Israel (Tel Aviv-Yafo) - Meetup', \
+		 'http://notapageatall.co.il/': 'Problem loading page', \
+		 'ftp://someftp': 'No matter'}
+
+driver = webdriver.Firefox()
+
+for link in links:
+	driver.get(host)
+	text_box = driver.find_element_by_name('link')
+	text_box.send_keys(link)
+	text_box.submit()
+
+	if driver.current_url == host + 'urls':
+		shortened_link = driver.find_element_by_partial_link_text('/l/')
+		shortened_link.click()
+		assert links[link] == driver.title
+	else:
+		assert driver.current_url == host + 'error'
+		parsed_link = urlparse(link)
+		assert parsed_link.scheme not in valid_protocols or not parsed_link.netloc
+
